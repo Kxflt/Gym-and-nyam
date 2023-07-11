b42cd973dc901a50ffcedcac58fb131187b1300e
Kxflt <arg.uri.solanes@gmail.com>
2023-07-11 18:31:45 +0200

    CSS Login, AuthContext and NavBar
    We arrenge a bit more our CSS login, almost finished. NavBar is started and authContext is finished

--- frontend/src/Components/Shared/logo/Logo.jsx
+++ frontend/src/Components/Shared/logo/Logo.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ImgLogo from '../../assets/logo2.png';
+import ImgLogo from '../../../assets/logo2.png';
+import './logo.css';
+
 function Logo() {
-  return (
-    <>
-      <img src={ImgLogo} alt='logo' />
-    </>
-  );
+  return;
+  <>
+    <img src={ImgLogo} alt='logo' />;
+  </>;
 }
 export default Logo;
--- frontend/src/Components/nav-bar/NavBar.jsx
+++ frontend/src/Components/nav-bar/NavBar.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useAuth } from '../../context/authContext';
-import Button from '../Shared/Button/Button';
+
 import './nav-bar.css';
 
 function NavBar() {
-  const { isAuthenticated, user, logOut } = useAuth();
   return (
     <nav className='header-container'>
-      {isAuthenticated ? (
-        <>
-          <div>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='exercises'>Exercises</NavLink>
-            <NavLink to='profile'>Profile</NavLink>
-          </div>
-          <div>
-            <span>{`Hola, ${user.name}`}</span>
-            <Button text='Log Out' onClick={logout} error />
-          </div>
-        </>
-      ) : (
-        <div>
-          <NavLink to='login'>Log In</NavLink>
-          <NavLink to='signup'>Sign Up</NavLink>
-        </div>
-      )}
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='exercises'>Exercises</NavLink>
+      <NavLink to='profile'>Profile</NavLink>
     </nav>
   );
 }
