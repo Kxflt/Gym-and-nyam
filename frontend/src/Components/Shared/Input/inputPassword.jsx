0b522c3e6b86fc8527c4ca2a67d4afdcfdcaa216
Marina Hernandez <marina.hernandez@lecol-044.home>
2023-07-13 20:18:47 +0100

    Newuser

--- frontend/src/Components/Shared/Input/inputPassword.jsx
+++ frontend/src/Components/Shared/Input/inputPassword.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import './input.css';
+import React from "react";
+import { string, object } from "prop-types";
+import "./input.css";
 
-function InputPassword() {
+function InputPassword({ label, register, errors, registerName }) {
   return (
     <>
-      <input type='password' placeholder='Password' className='password' />
+      <label>{label}</label>
+      <input type="password" {...register} />
+      {errors[registerName]?.type === "required" && (
+        <span className="error">Campo requerido</span>
+      )}
+      {errors[registerName]?.type === "minLength" && (
+        <span className="error">Mínimo 8 caracteres</span>
+      )}
     </>
   );
 }
+
+InputPassword.propTypes = {
+  label: string,
+  register: object,
+  errors: object,
+  registerName: string,
+};
 
 export default InputPassword;
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,8 @@
 export const EMAIL_REGEX =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const MAX_LENGTH_STRING = 30;
+export const MAX_LENGTH_STRING = 20;
+
+export const MIN_LENGTH_STRING = 3;
 
 export const CURRENT_USER_LOCAL_STORAGE = 'currentUser';
