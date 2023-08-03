d4e9936c951aefee3b322cf60d1906da1f8a26aa
Ryan John Quinn <ryanquinn23@live.co.uk>
2023-08-03 13:23:01 +0200

    aboutUs and minor css

--- frontend/src/Pages/dashboard/Dashboard.jsx
+++ frontend/src/Pages/dashboard/Dashboard.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
-
+import { NavLink, Navigate } from 'react-router-dom';
 import Button from '../../Components/Shared/Button/Button';
 import meal from '../../assets/meal.jpeg';
 import girl from '../../assets/girl.png';
 import banner2 from '../../assets/banner2.png';
+import ImageCarousel from '../../Components/Carousel/Carousel';
 
 import './dashboard.css';
 
-import ImageCarousel from '../../Components/Carousel/Carousel';
-
 function Dashboard() {
-  return (
-    <>
-      <ImageCarousel />
-      <div className="dashboard-container">
-        <main className="main-content">
-          {/* <div className="publi1">
+    return (
+        <>
+            <ImageCarousel />
+            <div className="dashboard-container">
+                <main className="main-content">
+                    {/* <div className="publi1">
             <img src={banner2} alt="imagenbanner" className="banner" />
           </div> */}
-          <Button text="ABOUT US" />
-          <div className="image-container">
-            <img src={girl} alt="secbanner1" className="banner2" />
-            <img src={meal} alt="secbanner2" className="banner3" />
-          </div>
-        </main>
-      </div>
-    </>
-  );
+                    <Button text="ABOUT US" />
+                    <div className="image-container">
+                        <img src={girl} alt="secbanner1" className="banner2" />
+                        <img src={meal} alt="secbanner2" className="banner3" />
+                    </div>
+                </main>
+            </div>
+        </>
+    );
 }
 
 export default Dashboard;
